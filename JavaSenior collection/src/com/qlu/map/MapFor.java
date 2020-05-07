deeac9969aa3e36cea1d3e51a75c8a68c3de8a19
jiajinshuo <1948369431@qq.com>
2020-05-07 14:50:11 +0800

    遍历map更新

---
+++ JavaSenior collection/src/com/qlu/map/MapFor.java
@@ -0,0 +1,21 @@
+package com.qlu.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jiajinshuo
+ * @create 2020-05-07 14:37
+ */
+public class MapFor {
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("1",1);
+        map.put("2",2);
+
+        // 遍历
+        for(Map.Entry<String,Object> entry:map.entrySet()){
+            System.out.println(entry.getKey()+"---------"+entry.getValue());
+        }
+    }
+}
--- JavaSenior collection/src/com/qlu/map/MapTest.java
+++ JavaSenior collection/src/com/qlu/map/MapTest.java
@@ -1,9 +1,6 @@
 package com.qlu.map;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author jiajinshuo
@@ -12,12 +9,12 @@ import java.util.Set;
 public class MapTest {
     public static void main(String[] args) {
 
-        Map map = new HashMap();
+        Map<String,Object> map = new HashMap<>();
         //添加
         map.put("AA",123);
         //修改
         map.put("AA",56);//key相同直接覆盖掉
-        map.put(456,12);
+        map.put("456",12);
         map.put("BB",22);
 
         System.out.println(map);
@@ -28,16 +25,27 @@ public class MapTest {
         //获取指定key的value
         System.out.println(map.get(456));
         System.out.println("************");
-        //遍历所有的key   keySet()
+
+        System.out.println("遍历所有的key   keySet()");
         Set set = map.keySet();
         Iterator iterator = set.iterator();
         while(iterator.hasNext()){
             System.out.println(iterator.next());
         }
-        //遍历key value。Set里面放了好多个entry
-        Set entry = map.entrySet();
-        for(Object i:entry){
+        System.out.println("遍历所有的value,遍历的顺序和key一致");
+        Collection values = map.values();
+        for(Object value: values){
+            System.out.println(value);
+        }
+        System.out.println("遍历key value。Set里面放了好多个entry");
+        Set entry01 = map.entrySet();
+        for(Object i:entry01){
             System.out.println(i);
+        }
+
+        //Map.Entry<k,v>,Entry是内部接口
+        for(Map.Entry<String,Object> entry:map.entrySet()){
+            System.out.println("key:"+entry.getKey()+"   value:"+entry.getValue());
         }
 
     }
